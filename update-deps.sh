486b96b90b6f469e1dcae8b7981fcdac286e4709
herveGuigoz <herve.guigoz@gmail.com>
2019-10-22 02:06:51 +0200

    add update-deps.sh

---
+++ update-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker-compose pull
+docker-compose build
+docker-compose run php composer update
+docker-compose run boo /bin/sh -c 'yarn install && yarn upgrade'
+cd api && composer sync-recipes --force
+echo 'Run `docker-compose up --build --force-recreate` now and check that everything is fine!'
