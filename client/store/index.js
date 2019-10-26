2b19a4524366ca25602017dd41013bd77abcded8
herveGuigoz <herve.guigoz@gmail.com>
2019-10-26 18:47:12 +0200

    we store only token in cookie

--- client/store/index.js
+++ client/store/index.js
@@ -1,4 +1,5 @@
-
+const jwtJsDecode = require('jwt-js-decode');
+const nowInSeconds = Date.now() / 1000;
 // State object
 export const state = () => ({
   user: null
@@ -6,32 +7,39 @@ export const state = () => ({
 
 // Getter functions
 export const getters = {
-  isAuth: state => state.user,
+  isAuth (state) {
+    return !(!state.user || nowInSeconds > state.user.expire);
+  },
   getUser: state => state.user,
-}
+};
 
 // Mutations
 export const mutations = {
   RESET (state) {
     state.user =  null;
   },
-  SET_USER (state, auth) {
-    state.user = auth
+  SET_USER (state, token) {
+    try {
+      const jwt = jwtJsDecode.jwtDecode(token);
+      const username = jwt.payload.username;
+      const expire = jwt.payload.exp;
+      nowInSeconds < expire ? state.user = { token, username , expire } : state.user = null
+    } catch (e) {
+      state.user =  null;
+    }
   }
-}
+};
 
 // Actions
 export const actions = {
   reset(state) {
     state.commit("RESET");
   },
-  auth(state, { token, username }) {
+  auth(state, token) {
     try {
       this.$cookie.removeAll()
-      const lastLogin = Date.now();
-      const user = { token, username, lastLogin }
-      this.$cookie.set('auth', user, { maxAge: 60 * 60 * 60 * 24 * 30 })
-      state.commit('SET_USER', user)
+      this.$cookie.set('auth', token, { maxAge: 60 * 60 * 60 * 24 * 30 })
+      state.commit('SET_USER', token)
     } catch (e) {
       throw new Error(e.message)
     }
