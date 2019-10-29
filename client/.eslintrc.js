54548ca49e83d7ea45bde57c212cba0f9de7f146
herveGuigoz <herve.guigoz@gmail.com>
2019-10-29 22:16:55 +0100

    step1

---
+++ client/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    '@nuxtjs'
+  ],
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    "no-console": 1 // Allows console.log
+  }
+}
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -72,6 +72,19 @@ export default {
       }
     },
     extend (config, ctx) {
+      /*
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+      */
     }
   },
   transition: {
