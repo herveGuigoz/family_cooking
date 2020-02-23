c1589644be3080f3331accf21e70f04b9f0235bd
herveGuigoz <herve.guigoz@gmail.com>
2020-02-23 19:56:09 +0100

    fix gitignore

--- client/.nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['auth'] = require('../middleware/auth.js')
-middleware['auth'] = middleware['auth'].default || middleware['auth']
-
-middleware['restricted'] = require('../middleware/restricted.js')
-middleware['restricted'] = middleware['restricted'].default || middleware['restricted']
-
-export default middleware
